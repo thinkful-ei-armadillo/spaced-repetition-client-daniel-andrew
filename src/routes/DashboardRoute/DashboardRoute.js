2cad5469ce2967a6727e9f7b6461960054241b07
andrewbituin <fandrewbituin@gmail.com>
2019-04-15 09:58:59 -0700

    functioning GET request to /language endpoint

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -5,46 +5,28 @@ import TokenService from '../../services/token-service';
 
 class DashboardRoute extends Component {
   state = {
-    languageName: '',
+    language: {},
+    words: []
   }
-  // getLanguage = () => {
-  //   fetch(`${config.API_ENDPOINT}/language`, {
-  //     method: 'GET',
-  //     header: {
-  //       "content-type": "application/json"
-  //     } 
-  //   })
-  //   .then(res => {
-  //     console.log(res);
-  //     return res.json();
-  //   })
-  //   .then(res => {
-  //     this.setState({ languageName: res.name })
-  //   })
-  // }
   componentDidMount(){
-    console.log(TokenService.getAuthToken())
     fetch(`${config.API_ENDPOINT}/language`, {
       method: 'GET',
-      header: {
+      headers: {
         "Authorization": `Bearer ${TokenService.getAuthToken()}`,
         "content-type": "application/json"
       }, 
     })
     .then(res => {
-      console.log(res);
       return res.json();
     })
     .then(res => {
-      this.setState({ languageName: res.name })
+      this.setState({ language: res.language, words: res.words })
     })
   }
   render() {
-    // this.getLanguage();
-    // let language = this.state.languageName;
     return (
       <section>
-        <h2>My {this.state.languageName} Dashboard</h2>
+        <h2>My {this.state.language.name} Dashboard</h2>
       </section>
     );
   }
