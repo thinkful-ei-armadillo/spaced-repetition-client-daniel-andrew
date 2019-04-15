2d9f8f50ad5906a794b13b958baf3e3ae22b1145
andrewbituin <fandrewbituin@gmail.com>
2019-04-15 09:45:53 -0700

    working on get request to language endpoint

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
 import './dashboard.css';
+import config from '../../config';
+import TokenService from '../../services/token-service';
 
 class DashboardRoute extends Component {
+  state = {
+    languageName: '',
+  }
+  // getLanguage = () => {
+  //   fetch(`${config.API_ENDPOINT}/language`, {
+  //     method: 'GET',
+  //     header: {
+  //       "content-type": "application/json"
+  //     } 
+  //   })
+  //   .then(res => {
+  //     console.log(res);
+  //     return res.json();
+  //   })
+  //   .then(res => {
+  //     this.setState({ languageName: res.name })
+  //   })
+  // }
+  componentDidMount(){
+    console.log(TokenService.getAuthToken())
+    fetch(`${config.API_ENDPOINT}/language`, {
+      method: 'GET',
+      header: {
+        "Authorization": `Bearer ${TokenService.getAuthToken()}`,
+        "content-type": "application/json"
+      }, 
+    })
+    .then(res => {
+      console.log(res);
+      return res.json();
+    })
+    .then(res => {
+      this.setState({ languageName: res.name })
+    })
+  }
   render() {
+    // this.getLanguage();
+    // let language = this.state.languageName;
     return (
       <section>
-        implement and style me
+        <h2>My {this.state.languageName} Dashboard</h2>
       </section>
     );
   }
