7104c511e26ba1166ad842dfba99a55b32e4b438
andrewbituin <fandrewbituin@gmail.com>
2019-04-16 09:48:24 -0700

    working on passing test for incorrect answer

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,44 +1,51 @@
-import React, { Component } from 'react';
-import config from '../../config';
-import TokenService from '../../services/token-service';
-import { Input, Label } from '../../components/Form/Form';
-import Button from '../../components/Button/Button';
-import './LearningRoute.css';
+import React, { Component } from "react";
+import config from "../../config";
+import TokenService from "../../services/token-service";
+import { Input, Label } from "../../components/Form/Form";
+import Button from "../../components/Button/Button";
+import "./LearningRoute.css";
 
 class LearningRoute extends Component {
   state = {
-    nextWord: '',
+    nextWord: "",
+    answer: '',
     totalScore: 0,
     wordCorrectCount: 0,
     wordIncorrectCount: 0,
-    guess: ''
-  }
+    guess: "",
+    result: ""
+  };
 
-  handleText = (event) => {
+  handleText = event => {
+    const submittedGuess = event.target.value;
     this.setState({
-      guess: event.target.value
-    })
-  }
+      guess: submittedGuess
+    });
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
 
-    console.log(this.state.guess);
-
-    fetch(`${config.API_ENDPOINT}/language/head`, {
+    console.log(this.state);
+    fetch(`${config.API_ENDPOINT}/language/guess`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${TokenService.getAuthToken()}`,
         "content-type": "application/json"
       },
-      body: {
-        guess: this.state.guess
-      }
+      body: JSON.stringify({ guess: this.state.guess })
     })
-    .then(res => {
-      return res.json;
-    })
-  }
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        if (res.answer === "Correct") {
+          this.setState({ result: "Correct" });
+        } else {
+          this.setState({ result: "Incorrect" });
+        }
+      });
+  };
 
   componentDidMount() {
     fetch(`${config.API_ENDPOINT}/language/head`, {
@@ -53,37 +60,79 @@ class LearningRoute extends Component {
       })
       .then(res => {
         console.log(res);
-        this.setState({ 
+        this.setState({
           nextWord: res.nextWord,
+          answer: res.correctAnswer,
           totalScore: res.totalScore,
           wordCorrectCount: res.wordCorrectCount,
           wordIncorrectCount: res.wordIncorrectCount
-         });
+        });
       });
   }
 
-
   render() {
     console.log(this.state);
+    let resultTemplate;
+    if (this.state.result === "Correct") {
+      resultTemplate = (
+        <h2 className="correct-result">Good try, but not quite right :(</h2>
+      );
+    }
+    if (this.state.result === "Incorrect") {
+      resultTemplate = (
+        <div className="DisplayFeedback">
+        <h2 className="incorrect-result">Good try, but not quite right :(</h2>
+        <p>The correct translation for {this.state.nextWord} was {this.state.answer} and you chose {this.state.guess}!</p>
+        </div>
+      );
+    }
     return (
       <section>
-        <h2>Translate the word:</h2><br/>
-        <span id='learn-word'>{this.state.nextWord}</span><br/><br/>
-        <p>Your total score is: {this.state.totalScore}</p><br/>
+        <h1>Translate the word:</h1>
+        <br />
+        <span id="learn-word">{this.state.nextWord}</span>
+        <br />
         <main>
-          <form className='learn-form' onSubmit={(e) => this.handleSubmit(e)}>
-            <Label htmlFor='learn-guess-input'>
+          <div className="DisplayScore">
+            <p>Your total score is: {this.state.totalScore}</p>
+          </div>
+          <br />
+          <br />
+          <div className="result-container">
+            {this.state.result ? resultTemplate : ""}
+          </div>
+          <form className="learn-form" onSubmit={e => this.handleSubmit(e)}>
+            <Label htmlFor="learn-guess-input">
               What's the translation for this word?
-            </Label><br/><br/>
-            <Input id='learn-guess-input' type='text' value={this.state.guess} onChange={(e) => this.handleText(e)} required/><br/><br/>
-            <Button type='submit' id='learn-submit-button' >Submit your answer</Button>
-          </form><br/>
-          <p>You have answered this word correctly {this.state.wordCorrectCount} times.</p>
-          <p>You have answered this word incorrectly {this.state.wordIncorrectCount} times.</p>
+            </Label>
+            <br />
+            <br />
+            <Input
+              id="learn-guess-input"
+              type="text"
+              value={this.state.guess}
+              onChange={e => this.handleText(e)}
+              required
+            />
+            <br />
+            <br />
+            <Button type="submit" id="learn-submit-button">
+              Submit your answer
+            </Button>
+          </form>
+          <br />
+          <p>
+            You have answered this word correctly {this.state.wordCorrectCount}{" "}
+            times.
+          </p>
+          <p>
+            You have answered this word incorrectly{" "}
+            {this.state.wordIncorrectCount} times.
+          </p>
         </main>
       </section>
     );
   }
 }
 
-export default LearningRoute
+export default LearningRoute;
