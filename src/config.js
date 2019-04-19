47040c010914f0069fb539f54db5226a54926ee3
Daniel Ors <do7811@gmail.com>
2019-04-19 12:19:45 -0400

    completed deploy - all done!

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  // API_ENDPOINT: 'https://blooming-shelf-49266.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://obscure-wave-29989.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
